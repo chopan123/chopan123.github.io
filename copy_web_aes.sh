787c36155d1a5e27996b8d141673f24ee040a5e1
chopan123 <chopancatrileo@gmail.com>
2020-06-02 23:23:54 -0400

    front aes added

---
+++ copy_web_aes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Copying files..."
+
+rm -r ~/workspace/chopan123.github.io/aes
+cp -R /home/chopan-laptop/workspace/front-aes/front_aes/build/web ~/workspace/chopan123.github.io/aes
+
+echo "Done!"
